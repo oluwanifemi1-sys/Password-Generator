18d8d74fa5ad14bf2b17e622ecd7dc4aaaa9231f
oluwanifemi1-sys <oreoluwa74@gmail.com>
2024-09-14 11:37:06 +0100

    finished the password generator project

---
+++ index.js
@@ -0,0 +1,115 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+let processorEl = document.getElementById("processor");
+let passwordEl = document.getElementById("password");
+
+let passwordLength = 15;
+
+function generateRandomCharacter() {
+  let randomCharacter = Math.floor(Math.random() * characters.length);
+  return characters[randomCharacter];
+}
+
+function getRandomPassword() {
+  let passwordOne = "";
+  let passwordTwo = "";
+  for (let i = 0; i < passwordLength; i++) {
+    passwordOne += generateRandomCharacter();
+    passwordTwo += generateRandomCharacter();
+  }
+  processorEl.textContent = passwordOne;
+  passwordEl.textContent = passwordTwo;
+}
+generateRandomCharacter();
